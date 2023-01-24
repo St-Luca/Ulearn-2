e083198b85d5511e785765bd4415297c008f572e
St-Luca <104420338+St-Luca@users.noreply.github.com>
2023-01-24 21:24:05 +0500

    Структуры данных
    Структуры данных

---
+++ 11. Структуры данных/BinaryTree.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace BinaryTrees
+{
+    public class TreeNode<T>
+    {
+        public TreeNode<T> Left;
+        public TreeNode<T> Right;
+        public T Value;
+
+        public TreeNode(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class BinaryTree<T> where T : IComparable
+    {
+        public TreeNode<T> RootTree;
+
+        public bool Contains(T element)
+        {
+            TreeNode<T> temp = RootTree;
+
+            while (temp != null)
+            {
+                if (temp.Value.CompareTo(element) == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (temp.Value.CompareTo(element) > 0) //значение после искомого
+                    {
+                        temp = temp.Right;
+                    }
+                    else //значение до искомого 
+                    {
+                        temp = temp.Left;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void Add(T element)
+        {
+            TreeNode<T> temp = RootTree;
+
+            if (temp == null) //Дерева/ветки еще не существует
+            {
+                RootTree = new TreeNode<T>(element);
+            }
+            else
+            {
+                while (true)
+                {
+                    if (temp.Value.CompareTo(element) < 0)
+                    {
+                        if (temp.Left != null)
+                        {
+                            temp = temp.Left;
+                        }
+                        else
+                        {
+                            temp.Left = new TreeNode<T>(element);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (temp.Right != null)
+                        {
+                            temp = temp.Right;
+                        }
+                        else
+                        {
+                            temp.Right = new TreeNode<T>(element);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 11. Структуры данных/DiskTreeTask.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace DiskTree
+{
+    public class DiskTreeTask
+    {
+        public static List<string> Solve(List<string> inputStrings)
+        {
+            Directory rootDirectory = new Directory("dir");
+
+            foreach (string line in inputStrings)
+            {
+                string[] directories = line.Split('\\');
+                Directory currentDir = rootDirectory;
+
+                foreach (string directory in directories)
+                {
+                    currentDir = currentDir.FindDirectory(directory);
+                }
+            }
+
+            return rootDirectory.OrderDirectories(0, new List<string>());
+        }
+    }
+
+    public class Directory
+    {
+        public string DirectoryName;         //Словарь директорий, которые после текущей
+        public Dictionary<string, Directory> DirectoriesDict = new Dictionary<string, Directory>();
+
+        public Directory(string directoryName)
+        {
+            DirectoryName = directoryName;
+        }
+
+        public Directory FindDirectory(string directory)
+        {
+            if (DirectoriesDict.TryGetValue(directory, out Directory resDirectory))
+            {
+                return resDirectory;
+            }
+
+            DirectoriesDict[directory] = new Directory(directory);
+
+            return DirectoriesDict[directory];
+        }
+
+        public List<string> OrderDirectories(int spaces, List<string> directoriesList)
+        {
+            if (DirectoryName != "dir")
+            {
+                directoriesList.Add(new string(' ', spaces) + DirectoryName);
+                spaces++;
+            }
+
+            foreach (Directory directory in DirectoriesDict.Values.OrderBy(d => d.DirectoryName, StringComparer.Ordinal))
+            {
+                directoriesList = directory.OrderDirectories(spaces, directoriesList);
+            }
+
+            return directoriesList;
+        }
+    }
+}
