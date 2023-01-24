2a130ac2431bf02e9a6e28afe98a887165a68dc1
St-Luca <104420338+St-Luca@users.noreply.github.com>
2023-01-24 20:51:39 +0500

    Очереди, стеки, дженерики
    Очереди, стеки, дженерики

---
+++ 1. Очереди, стеки, дженерики/LimitedSizeStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        LinkedList<T> list = new LinkedList<T>();
+        int limit;
+
+        public LimitedSizeStack(int limit)
+        {
+            this.limit = limit;
+        }
+
+        public void Push(T item)
+        {
+            list.AddLast(item);
+            if (list.Count > limit)
+            {
+                list.RemoveFirst();
+            }
+        }
+
+        public T Pop()
+        {
+            var lastVal = list.Last.Value;
+            list.RemoveLast();
+            return lastVal;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+    }
+}
+
---
+++ 1. Очереди, стеки, дженерики/ListModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class ListModel<TItem>
+    {
+        public List<TItem> Items { get; }
+        public int Limit;
+        public LimitedSizeStack<Tuple<CommandType, TItem, int>> LSStack; //В кортеже хранится какая это команда, элемент и индекс элемента
+
+        public ListModel(int limit)
+        {
+            LSStack = new LimitedSizeStack<Tuple<CommandType, TItem, int>>(limit);
+            Items = new List<TItem>();
+            Limit = limit;
+        }
+
+        public enum CommandType
+        {
+            RemoveItem,
+            AddItem
+        }
+
+        public void RemoveItem(int index)
+        {
+            LSStack.Push(Tuple.Create(CommandType.RemoveItem, Items[index], index));
+            Items.RemoveAt(index);
+        }
+
+        public void AddItem(TItem item)
+        {
+            Items.Add(item);
+            LSStack.Push(Tuple.Create(CommandType.AddItem, item, Items.Count - 1));
+        }
+
+        public bool CanUndo()
+        {
+            return LSStack.Count > 0;
+        }
+
+        public void Undo()
+        {
+            var (commandType, item, index) = LSStack.Pop();
+            if (commandType == CommandType.RemoveItem)
+            {
+                if (LSStack.Count == 1)
+                {
+                    Items.Insert(index - 1, item);
+                }
+                else
+                {
+                    Items.Insert(index, item);
+                }
+            }
+            else if (commandType == CommandType.AddItem)
+            {
+                Items.RemoveAt(LSStack.Count);
+            }
+        }
+    }
+}
